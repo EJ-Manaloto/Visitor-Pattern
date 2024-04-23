bd915b5e6e1347932e0945690e65ce6a9906fcbf
EJ-Manaloto <112523625+EJ-Manaloto@users.noreply.github.com>
2024-04-23 23:26:46 +0800

    Create TelcoSubscription.java

---
+++ TelcoSubscription.java
@@ -0,0 +1,15 @@
+interface TelcoSubscription 
+{
+
+  public String accept(UsagePromo promo, double price);
+
+  public String accept(UnliCallsTextOffer unliPackage, boolean unliCallText);
+
+  public String getTelcoName();
+
+  public double getPromoPrice();
+
+  public int getDataAllowance();
+
+  public boolean getUnliCallText();
+}
