20d3d1fa9fcd41da0d2787d7ee86270b872db50b
EJ-Manaloto <112523625+EJ-Manaloto@users.noreply.github.com>
2024-04-23 23:27:28 +0800

    Create TelcoAllowance.java

---
+++ TelcoAllowance.java
@@ -0,0 +1,27 @@
+class TelcoAllowance implements UsagePromo {
+
+  @Override
+  public String showAllowance(String telcoName, double price) {
+    return telcoName + " offers " + getDataAllowance(telcoName, price) + " GB data allowance for Php " + price + " per month."; // Replace with your logic to retrieve dataAllowance based on telcoName and price (if needed)
+  }
+
+  private int getDataAllowance(String telcoName, double price) {
+
+    if (telcoName.equals("Smart")) 
+    {
+      return 15;
+    } 
+    else if (telcoName.equals("Globe")) 
+    {
+      return 10;
+    } 
+    else if (telcoName.equals("Ditto")) 
+    {
+      return 8;
+    } 
+    else
+    {
+      return 0;
+    }
+  }
+}
