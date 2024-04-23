23225dd72968f71b2babd19ef420d89f0c77d3bf
EJ-Manaloto <112523625+EJ-Manaloto@users.noreply.github.com>
2024-04-23 23:29:31 +0800

    UnliCallTextPackage.java

--- UnliCallTextPackage.java
+++ UnliCallTextPackage.java
@@ -3,7 +3,7 @@ class UnliCallTextPackage implements UnliCallsTextOffer {
   @Override
   public String showUnliCallsTextOffer(String telcoName, boolean unliCallText) {
     if (unliCallText) {
-      return telcoName + " offers unlimited calls and texts."; // Added return statement
+      return telcoName + " offers unlimited calls and texts.";
     } else {
       return telcoName + " does not offer unlimited calls and texts.";
     }
